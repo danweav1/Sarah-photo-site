c340f0400e250b99ca1728c0a8d978dc3484ddd8
danweav1 <danweaver12@gmail.com>
2021-04-14 23:48:18 +0200

    increased nav menu font size, added transparent background to nav menu, added current page color nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        aqua: '#00F7FF'
+      },
       fontFamily: {
         body: ['Nunito']
       }
